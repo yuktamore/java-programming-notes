e560074eb2cadb2210512f55b92d5018583f1cd0
Yukta More <68540038+yuktamore@users.noreply.github.com>
2026-08-24 21:53:16 +0530

    Create firstProgram.java

---
+++ basics/firstProgram.java
@@ -0,0 +1,9 @@
+//defined a class named 'firstProgram'
+public class firstProgram
+{ //opening bracket for class
+
+  //main method: entry point
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
